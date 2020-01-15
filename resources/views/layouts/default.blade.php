659b362c14634bcba0718eb0d1d136beead2efbf
Append <appendtrue@gmail.com>
2020-01-15 18:04:12 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title', getenv('APP_NAME'). ' App') -Laravel 入门教程</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '主页')
+
+@section('content')
+  <h1>通用主页</h1>
+@stop
